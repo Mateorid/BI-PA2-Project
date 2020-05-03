81183083d93d76611aaf7506cde57de918196479
Mateorid <gorgomat@fit.cvut.cz>
2020-05-03 21:10:47 +0200

    Initial commit.

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -3,9 +3,9 @@
 /**Title of the game*/
 const char *const GAME_NAME = "Tanks of World";
 /**Game window width*/
-const int GAME_WIDTH = 1280;
+const int GAME_WIDTH = 800;
 /**Game window height*/
-const int GAME_HEIGHT = 720;
+const int GAME_HEIGHT = 600;
 /**Game FPS limit*/
 const int MAX_FPS = 60;
 /**Start game in fullscreen*/
